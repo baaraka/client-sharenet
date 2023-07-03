15c0dd74c755cbe7e08c097e8534ccaa4913e192
barakaganai <barakaganai01@gmail.com>
2023-07-03 16:08:31 +0300

    import pages into the app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
+import Settings from "./pages/settings/Settings";
+import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/singlePost" element={<Single />} />
+          <Route path="/write" element={<Write />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
