0afa1c7706ed7de7c271e8737c098eaedfed9fd5
barakaganai <barakaganai01@gmail.com>
2023-07-03 16:12:13 +0300

    create single.jsx

---
+++ src/pages/single/Single.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SinglePost from "../../components/singlePost/SinglePost";
+import NavBar from "../../components/navBar/NavBar";
+
+function Single() {
+  return (
+    <div>
+      <NavBar />
+      <SinglePost />
+    </div>
+  );
+}
+
+export default Single;
