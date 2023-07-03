2947750da903141418bb6afd64b2a893d6c7e8fd
barakaganai <barakaganai01@gmail.com>
2023-07-03 16:09:49 +0300

    create Share.jsx

---
+++ src/components/share/Share.jsx
@@ -0,0 +1,116 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import "./Share.scss";
+
+function Share() {
+  return (
+    <div className="share">
+      <div className="shareContainer">
+        <div className="shareContainerList">
+          <img
+            src="https://images.pexels.com/photos/17168353/pexels-photo-17168353/free-photo-of-balloons-flying-in-cappadocia-in-morning.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+            alt=""
+          />
+          <h1>Flying by balloons</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente,
+            beatae asperiores. Optio, ipsa saepe rem at autem similique, atque,
+            doloribus fugit nesciunt voluptatem alias. Nostrum adipisci modi
+            magnam eaque deserunt?
+          </p>
+          <div className="likeCount">
+            <FontAwesomeIcon icon={faHeart} />
+            <span>5</span>
+          </div>
+        </div>
+        <div className="shareContainerList">
+          <img
+            src="https://images.pexels.com/photos/17168353/pexels-photo-17168353/free-photo-of-balloons-flying-in-cappadocia-in-morning.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+            alt=""
+          />
+          <h1>Flying by balloons</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente,
+            beatae asperiores. Optio, ipsa saepe rem at autem similique, atque,
+            doloribus fugit nesciunt voluptatem alias. Nostrum adipisci modi
+            magnam eaque deserunt?
+          </p>
+          <div className="likeCount">
+            <FontAwesomeIcon icon={faHeart} />
+            <span>5</span>
+          </div>
+        </div>
+        <div className="shareContainerList">
+          <img
+            src="https://images.pexels.com/photos/17168353/pexels-photo-17168353/free-photo-of-balloons-flying-in-cappadocia-in-morning.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+            alt=""
+          />
+          <h1>Flying by balloons</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente,
+            beatae asperiores. Optio, ipsa saepe rem at autem similique, atque,
+            doloribus fugit nesciunt voluptatem alias. Nostrum adipisci modi
+            magnam eaque deserunt?
+          </p>
+          <div className="likeCount">
+            <FontAwesomeIcon icon={faHeart} />
+            <span>5</span>
+          </div>
+        </div>
+        <div className="shareContainerList">
+          <img
+            src="https://images.pexels.com/photos/17168353/pexels-photo-17168353/free-photo-of-balloons-flying-in-cappadocia-in-morning.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+            alt=""
+          />
+          <h1>Flying by balloons</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente,
+            beatae asperiores. Optio, ipsa saepe rem at autem similique, atque,
+            doloribus fugit nesciunt voluptatem alias. Nostrum adipisci modi
+            magnam eaque deserunt?
+          </p>
+          <div className="likeCount">
+            <FontAwesomeIcon icon={faHeart} />
+            <span>5</span>
+          </div>
+        </div>
+        <div className="shareContainerList">
+          <img
+            src="https://images.pexels.com/photos/17168353/pexels-photo-17168353/free-photo-of-balloons-flying-in-cappadocia-in-morning.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+            alt=""
+          />
+          <h1>Flying by balloons</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente,
+            beatae asperiores. Optio, ipsa saepe rem at autem similique, atque,
+            doloribus fugit nesciunt voluptatem alias. Nostrum adipisci modi
+            magnam eaque deserunt?
+          </p>
+          <div className="likeCount">
+            <FontAwesomeIcon icon={faHeart} />
+            <span>5</span>
+          </div>
+        </div>
+        <div className="shareContainerList">
+          <img
+            src="https://images.pexels.com/photos/17168353/pexels-photo-17168353/free-photo-of-balloons-flying-in-cappadocia-in-morning.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+            alt=""
+          />
+          <h1>Flying by balloons</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente,
+            beatae asperiores. Optio, ipsa saepe rem at autem similique, atque,
+            doloribus fugit nesciunt voluptatem alias. Nostrum adipisci modi
+            magnam eaque deserunt?
+          </p>
+          <div className="likeCount">
+            <FontAwesomeIcon icon={faHeart} />
+            <span>5</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Share;
