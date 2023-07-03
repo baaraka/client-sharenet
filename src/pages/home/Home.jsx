5c154469e145d329d93153dd69c59ac2332ee74b
barakaganai <barakaganai01@gmail.com>
2023-07-03 16:10:45 +0300

    create home.jsx

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "../../components/navBar/NavBar";
+import Share from "../../components/share/Share";
+
+function Home() {
+  return (
+    <div>
+      <NavBar />
+      <Share />
+    </div>
+  );
+}
+
+export default Home;
