8fa71566c1fcf64a58b823f3d234ac5de8242131
barakaganai <barakaganai01@gmail.com>
2023-07-05 11:45:18 +0300

    fetch singlePost with specific id from the db

--- src/components/singlePost/SinglePost.jsx
+++ src/components/singlePost/SinglePost.jsx
@@ -5,41 +5,53 @@ import {
   faPenToSquare,
   faTrash,
 } from "@fortawesome/free-solid-svg-icons";
+import { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { useLocation } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
 
 function SinglePost() {
+  const [item, setItem] = useState({});
+
+  const { user } = useContext(AuthContext);
+
+  const pf = "http://localhost:5000/images/";
+
+  const location = useLocation();
+
+  const id = location.pathname.split("/")[2];
+
+  useEffect(() => {
+    const getPost = async () => {
+      const res = await axios.get(`http://localhost:5000/api/shares/${id}`);
+      setItem(res.data);
+    };
+    getPost();
+  }, [id]);
+
   return (
     <div className="singlePost">
       <div className="singlePostContainer">
-        <div className="singlePostContainerList">
-          <img
-            src="https://images.pexels.com/photos/17392069/pexels-photo-17392069/free-photo-of-fujifilm-camara-pexels.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
-            alt=""
-          />
-          <div className="singleItem">
-            <span>author: Barack</span>
-            <div className="singlePostItemList">
-              <FontAwesomeIcon icon={faPenToSquare} />
-              <FontAwesomeIcon icon={faTrash} />
+        {item && (
+          <div className="singlePostContainerList">
+            <img src={pf + item.image} alt="" />
+            <div className="singleItem">
+              <span>author: {item.username}</span>
+              {item.username === user?.username && (
+                <div className="singlePostItemList">
+                  <FontAwesomeIcon icon={faPenToSquare} />
+                  <FontAwesomeIcon icon={faTrash} />
+                </div>
+              )}
+            </div>
+            <h1>{item.title}</h1>
+            <p>{item.desc}</p>
+            <div className="likeCount">
+              <FontAwesomeIcon icon={faHeart} />
+              <span>{item.like}</span>
             </div>
           </div>
-          <h1>Flying by balloons</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente,
-            beatae asperiores. Optio, ipsa saepe rem at autem similique, atque,
-            doloribus fugit nesciunt voluptatem alias. Nostrum adipisci modi
-            magnam eaque deserunt? Lorem ipsum dolor sit amet consectetur,
-            adipisicing elit. Corrupti quas maiores iste quo veniam error a hic
-            enim eveniet voluptates molestiae minus atque harum amet, magni ex.
-            Et, nobis eum? Lorem ipsum dolor sit amet consectetur, adipisicing
-            elit. Eum sint iusto deserunt, veritatis labore assumenda,
-            consectetur dicta laudantium libero, perferendis recusandae nobis
-            qui. Accusantium deserunt at provident minus harum sapiente.
-          </p>
-          <div className="likeCount">
-            <FontAwesomeIcon icon={faHeart} />
-            <span>5</span>
-          </div>
-        </div>
+        )}
       </div>
     </div>
   );
