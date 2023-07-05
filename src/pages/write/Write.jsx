b7422c43af9716f5958d63c6a118f07e5e02d4c7
barakaganai <barakaganai01@gmail.com>
2023-07-05 11:47:24 +0300

    implement createPost

--- src/pages/write/Write.jsx
+++ src/pages/write/Write.jsx
@@ -1,23 +1,69 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./Write.scss";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import NavBar from "../../components/navBar/NavBar";
+import { useContext, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
 
 function Write() {
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [file, setFile] = useState("");
+
+  const { user } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  const handlePublish = async (e) => {
+    e.preventDefault();
+
+    const newPost = {
+      username: user.username,
+      title,
+      desc,
+    };
+    if (file) {
+      const data = new FormData();
+      const filename = Date.now() + file.name;
+      data.append("name", filename);
+      data.append("file", file);
+      newPost.image = filename;
+
+      try {
+        await axios.post("http://localhost:5000/api/upload", data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    try {
+      await axios.post("http://localhost:5000/api/shares", newPost);
+      navigate("/");
+    } catch (error) {}
+  };
+
   return (
     <>
       <NavBar />
       <div className="write">
         <div className="writeContainer">
-          <img
-            src="https://images.pexels.com/photos/17168353/pexels-photo-17168353/free-photo-of-balloons-flying-in-cappadocia-in-morning.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
-            alt=""
-          />
-          <form action="">
-            <input type="file" />
-            <input type="text" placeholder="Title" />
-            <textarea rows="5" placeholder="Share anything..."></textarea>
-            <button>Publish</button>
+          {file && <img src={URL.createObjectURL(file)} alt="" />}
+          <form onSubmit={handlePublish} encType="multipart/form-data">
+            <input
+              type="file"
+              name="image"
+              onChange={(e) => setFile(e.target.files[0])}
+            />
+            <input
+              type="text"
+              placeholder="Title"
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <textarea
+              rows="5"
+              placeholder="Share anything..."
+              onChange={(e) => setDesc(e.target.value)}
+            ></textarea>
+            <button type="submit">Publish</button>
           </form>
         </div>
       </div>
