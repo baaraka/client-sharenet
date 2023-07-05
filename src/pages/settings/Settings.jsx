6f29361825056a20a19594ceb5a3df81a104c34d
barakaganai <barakaganai01@gmail.com>
2023-07-05 16:48:31 +0300

    handle updateAccount  and deleteAccount

--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -1,7 +1,67 @@
+import { useContext, useState } from "react";
 import NavBar from "../../components/navBar/NavBar";
 import "./Settings.scss";
+import { AuthContext } from "../../context/AuthContext";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function Settings() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [file, setFile] = useState("");
+
+  const navigate = useNavigate();
+
+  const pf = "http://localhost:5000/images/";
+
+  const { user, dispatch } = useContext(AuthContext);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    dispatch({ type: "UPDATE_START" });
+
+    const updatedUser = {
+      userId: user._id,
+      username,
+      email,
+    };
+    if (file) {
+      const data = new FormData();
+      const filename = Date.now() + file.name;
+      data.append("name", filename);
+      data.append("file", file);
+      updatedUser.profilePic = filename;
+
+      try {
+        await axios.post("http://localhost:5000/api/upload", data);
+      } catch (error) {}
+    }
+
+    try {
+      const res = await axios.put(
+        `http://localhost:5000/api/users/${user._id}`,
+        updatedUser
+      );
+
+      dispatch({ type: "UPDATE_SUCCESS", payload: res.data });
+      navigate("/");
+    } catch (error) {
+      dispatch({ type: "UPDATE_FAILURE", payload: error.message });
+    }
+  };
+
+  const handleDelete = async () => {
+    try {
+      await axios.delete(`http://localhost:5000/api/users/${user._id}`, {
+        data: { userId: user._id },
+      });
+      dispatch({ type: "DELETE" });
+      navigate("/register");
+    } catch (error) {}
+  };
+
   return (
     <>
       <NavBar />
@@ -9,18 +69,26 @@ function Settings() {
         <div className="settingsContainer">
           <div className="containerUpdate">
             <h1>Update Your Account</h1>
-            <h2>Delete Your Account</h2>
+            <h2 onClick={handleDelete}>Delete Your Account</h2>
           </div>
           <img
-            src="https://images.pexels.com/photos/15577335/pexels-photo-15577335/free-photo-of-landscape-people-woman-legs.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+            src={file ? URL.createObjectURL(file) : pf + user.profilePic}
             alt=""
           />
-          <form action="">
-            <input type="file" />
-            <input type="text" placeholder="username" />
-            <input type="email" placeholder="email" />
+          <form action="" onSubmit={handleSubmit}>
+            <input type="file" onChange={(e) => setFile(e.target.files[0])} />
+            <input
+              type="text"
+              placeholder={user.username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <input
+              type="email"
+              placeholder={user.email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
             <input type="password" placeholder="password" />
-            <button>Update</button>
+            <button type="submit">Update</button>
           </form>
         </div>
       </div>
