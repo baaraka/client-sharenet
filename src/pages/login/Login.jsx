e01797e653e68017a95beba77daaa03ee695db74
barakaganai <barakaganai01@gmail.com>
2023-07-05 11:46:56 +0300

    implement login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import NavBar from "../../components/navBar/NavBar";
+import { AuthContext } from "../../context/AuthContext";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+  const { loading, error, dispatch } = useContext(AuthContext);
+
+  const handleChange = (e) => {
+    setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    dispatch({ type: "LOGIN_START" });
+
+    try {
+      const res = await axios.post(
+        "http://localhost:5000/api/auth/login",
+        credentials
+      );
+      dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+      navigate("/");
+    } catch (error) {
+      dispatch({ type: "LOGIN_FAILURE", payload: error.response.data.message });
+    }
+  };
   return (
     <>
       <NavBar />
@@ -12,13 +43,26 @@ function Login() {
           <form action="">
             <div className="form">
               <label htmlFor="">Username</label>
-              <input type="text" placeholder="eg Barack" />
+              <input
+                type="text"
+                id="username"
+                placeholder="eg Barack"
+                onChange={handleChange}
+              />
             </div>
             <div className="form">
               <label htmlFor="">Password</label>
-              <input type="password" placeholder="eg 1234ABC" />
+              <input
+                type="password"
+                id="password"
+                placeholder="eg 1234ABC"
+                onChange={handleChange}
+              />
             </div>
-            <button>Login</button>
+            <button disabled={loading} onClick={handleLogin}>
+              Login
+            </button>
+            {error && <span>{error}</span>}
           </form>
         </div>
       </div>
