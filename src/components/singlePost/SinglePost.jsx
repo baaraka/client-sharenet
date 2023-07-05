bd62967325d253c6fb604d8362c712dbd37e37e1
barakaganai <barakaganai01@gmail.com>
2023-07-05 13:47:47 +0300

    implement update post

--- src/components/singlePost/SinglePost.jsx
+++ src/components/singlePost/SinglePost.jsx
@@ -12,6 +12,9 @@ import { AuthContext } from "../../context/AuthContext";
 
 function SinglePost() {
   const [item, setItem] = useState({});
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [updatedMode, setUpdatedMode] = useState(false);
 
   const { user } = useContext(AuthContext);
 
@@ -25,9 +28,22 @@ function SinglePost() {
     const getPost = async () => {
       const res = await axios.get(`http://localhost:5000/api/shares/${id}`);
       setItem(res.data);
+      setTitle(res.data.title);
+      setDesc(res.data.desc);
     };
     getPost();
   }, [id]);
+
+  const handleUpdate = async () => {
+    try {
+      await axios.put(`http://localhost:5000/api/shares/${id}`, {
+        username: user.username,
+        title,
+        desc,
+      });
+      setUpdatedMode(false);
+    } catch (error) {}
+  };
 
   return (
     <div className="singlePost">
@@ -39,13 +55,33 @@ function SinglePost() {
               <span>author: {item.username}</span>
               {item.username === user?.username && (
                 <div className="singlePostItemList">
-                  <FontAwesomeIcon icon={faPenToSquare} />
+                  <FontAwesomeIcon
+                    icon={faPenToSquare}
+                    onClick={() => setUpdatedMode(true)}
+                  />
                   <FontAwesomeIcon icon={faTrash} />
                 </div>
               )}
             </div>
-            <h1>{item.title}</h1>
-            <p>{item.desc}</p>
+            {updatedMode ? (
+              <input
+                type="text"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            ) : (
+              <h1>{item.title}</h1>
+            )}
+            {updatedMode ? (
+              <textarea
+                rows="5"
+                value={desc}
+                onChange={(e) => setDesc(e.target.value)}
+              ></textarea>
+            ) : (
+              <p>{item.desc}</p>
+            )}
+            {updatedMode && <button onClick={handleUpdate}>Update Post</button>}
             <div className="likeCount">
               <FontAwesomeIcon icon={faHeart} />
               <span>{item.like}</span>
