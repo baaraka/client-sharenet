e1b2ce1fdc8c25749fcbaed9f48d2d83e8896c38
barakaganai <barakaganai01@gmail.com>
2023-07-05 11:47:10 +0300

    implement register

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,35 +1,78 @@
+import { useContext, useState } from "react";
 import NavBar from "../../components/navBar/NavBar";
 import "./Register.scss";
+import { AuthContext } from "../../context/AuthContext";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function Register() {
+  const [credentials, setCredentials] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  const { loading, dispatch } = useContext(AuthContext);
+
+  const handleChange = (e) => {
+    setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+
+    dispatch({ type: "REGISTER_START" });
+
+    try {
+      const res = await axios.post(
+        "http://localhost:5000/api/auth/register",
+        credentials
+      );
+      dispatch({ type: "REGISTER_SUCCESS", payload: res.data });
+      navigate("/login");
+    } catch (error) {
+      dispatch({ type: "REGISTER_FAILURE", payload: error.message });
+    }
+  };
   return (
     <>
       <NavBar />
       <div className="register">
         <div className="registerContainer">
           <h1>Register</h1>
-          <img
-            src="https://images.pexels.com/photos/15577335/pexels-photo-15577335/free-photo-of-landscape-people-woman-legs.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
-            alt=""
-          />
           <form action="">
-            <div className="form pp">
-              <label htmlFor="">Profile Picture</label>
-              <input type="file" />
-            </div>
             <div className="form">
               <label htmlFor="">Username</label>
-              <input type="text" placeholder="eg Barack" />
+              <input
+                type="text"
+                id="username"
+                placeholder="eg Barack"
+                onChange={handleChange}
+              />
             </div>
             <div className="form">
               <label htmlFor="">Email</label>
-              <input type="email" placeholder="eg barakaganai01@gmail.com" />
+              <input
+                type="email"
+                id="email"
+                placeholder="eg barakaganai01@gmail.com"
+                onChange={handleChange}
+              />
             </div>
             <div className="form">
               <label htmlFor="">Password</label>
-              <input type="password" placeholder="eg 1234ABC" />
+              <input
+                type="password"
+                id="password"
+                placeholder="eg 1234ABC"
+                onChange={handleChange}
+              />
             </div>
-            <button>Register</button>
+            <button disabled={loading} onClick={handleRegister}>
+              Register
+            </button>
           </form>
         </div>
       </div>
